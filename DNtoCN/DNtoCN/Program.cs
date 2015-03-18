18584f9450a475e1ec51377ab2ef01c83a817952
alwilliamsII <albert.l.williams@jsums.edu>
2015-03-18 15:01:54 -0500

    Working

--- DNtoCN/DNtoCN/Program.cs
+++ DNtoCN/DNtoCN/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
 
 namespace DNtoCN
 {
     class Program
     {
         static void Main(string[] args)
-        {//Test 
+        {
+            /*
+             * The purpse of this program is to filmerize my self with GitHub
+                Make a Function that takes a DN or Distinguished Name.
+          * 
+          */
+
+            string inputDN = Console.ReadLine();
+            string outputCN = DNtoCNa(inputDN);
+                
+            Console.WriteLine(outputCN);
+          
         }
+
+        static public string DNtoCNa (String dn)
+        {
+            
+            try {
+            string cn = dn.Substring(dn.IndexOf("=")+1,dn.IndexOf(",")-3);
+            return cn;
+ 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return  "None";
+               
+            }
+                     
+        }
+
+
     }
 }
